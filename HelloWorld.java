5396266657daf62bf5e293faaa17beba3c5cd2fd
Vino <Vino@192-168-1-108.tpgi.com.au>
2020-10-17 22:48:31 +1100

    Commiting practice

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld{
 
 	public static void main(String[] arg){
 		System.out.println("Hello Jenkins!!!");
+		System.out.println("Practice -1");
 		System.out.println("Polling Works !! Yay!!!!");
 	}
 
